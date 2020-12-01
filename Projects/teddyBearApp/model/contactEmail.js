be2f876aa56054caddd3e7b671cce340a7029f51
ASREEN <60615177+ASREEN@users.noreply.github.com>
2020-12-01 09:43:30 +0100

    remove project

--- Projects/teddyBearApp/model/contactEmail.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require('mongoose')
-var emailSchema = new mongoose.Schema({
-    email:String, 
-    date: { type: Date, default: Date.now }
-});
-
-var contactEmail = mongoose.model('contactEmail', emailSchema);
-module.exports=contactEmail;
